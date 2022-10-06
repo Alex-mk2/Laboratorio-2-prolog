4d5a18840e1c2746171db7dc5443ac9e455d782c
Alex Mellado <alex.mellado.g@usach.cl>
2022-10-06 17:48:20 -0300

    Primer commit, construccion TDA pixbit-d

---
+++ Laboratorio2Prolog.pl
@@ -0,0 +1,28 @@
+/*------------------------------------------------------
+         _          _      ____      ____          
+ ----   | |        / \    | __ )    |___ \    -------  
+ ----   | |       / _ \   |  _ \      __) |   -------
+ ----   | |___   / ___ \  | |_) |    / __/    -------
+ ----   |_____| /_/   \_\ |____/    |_____|   -------
+ --------------------------------------------------------
+/
+Nombre: Alex Mellado Gamboa
+Sección: 
+Profesor: Miguel Truffa
+Asignatura: Paradigmas de la programacion
+Rut: 19.684.813-4
+
+
+-------------------------Constructores-------------------
+Predicado image (Constructor de imagen)
+Dominio: Width(int) X Height(int) X [pixbit-d | pixrgb-d | pixhex-d] Aridad 5
+Recorrido: Image
+Metas primaria: pixbit-d, pixrgb-d, pixhex-d, alto, ancho
+Meta Secundarias: Imagen
+
+Predicado pixbit-d
+Dominio: X(int) X Y(int) X Bit([0|1]) X Depth(int) Aridad 4
+Recorrido: pixbit-d
+Metas primaria: pixbit-d, Bit
+Metas Secundarias: Imagen
+*/
