6aa64e4a85f35f1646581e1efff19eff71a0fc46
Alex Mellado <alex.mellado.g@usach.cl>
2022-10-09 16:35:37 -0300

    Avance constructores pixbit-d, pixrgb-d, pixhex-d

--- Laboratorio2Prolog.pl
+++ Laboratorio2Prolog.pl
@@ -25,4 +25,41 @@ Dominio: X(int) X Y(int) X Bit([0|1]) X Depth(int) Aridad 4
 Recorrido: pixbit-d
 Metas primaria: pixbit-d, Bit
 Metas Secundarias: Imagen
+
+Predicado pixrgb-d
+Dominio: X(int) X Y(int) X R(C) X G(C) X B(C) X D
+Recorrido: pixrgb-d
+Meta primaria: pixrgb-d
+Meta secundaria: imagen
+
+Predicado pixhex-d
+Dominio: X(int) X Y(int) X hex(string) X D(int)
+Recorrido: pixhex-d
+Meta primaria: pixhex-d
+Meta secundaria: imagen
 */
+
+
+/*Constructor pixbit-d
+Dom: X(int) X Y(int) X Bit([0|1]) X Depth(int)
+Rec: pixbit-d
+Ejemplo uso: 
+pixbit-d(10, 10, 1, 5, Lista). Devuelve Lista = [10, 10, 1, 5].
+*/
+pixbit-d(X, Y, Bit, Depth, [X,Y,Bit,Depth]).
+
+
+/*Constructor pixrgb-d
+Dom: X(int) X Y(int) X R(C) X G(C) X B(C) X D
+Rec: pixrgb-d
+Ejemplo uso:
+pixrgb-d(10, 10, 255, 255,255, 10, Lista), Devuelve Lista = [10, 10, 255, 255, 255, 10].
+*/
+pixrgb-d(X, Y, R, G, B, D, [X, Y, R, G, B, D]).
+
+/*Constructor pixhex-d
+Dom: Dominio: X(int) X Y(int) X hex(string) X D(int)
+Rec: pixhex-d
+Ejemplo uso: pixhex-d(10, 10, "#FF5733", 30, Lista), Devuelve lista = [10, 10, "#FF5733", 30].
+*/
+pixhex-d(X, Y, Hex, D, [X, Y, Hex, D]).
