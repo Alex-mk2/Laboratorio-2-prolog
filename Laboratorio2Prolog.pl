03bd2bae9f33a63906c27e409413d6932a189efb
Alex Mellado <alex.mellado.g@usach.cl>
2022-10-10 17:56:52 -0300

    Avance constructor imagen

--- Laboratorio2Prolog.pl
+++ Laboratorio2Prolog.pl
@@ -11,6 +11,7 @@ Sección:
 Profesor: Miguel Truffa
 Asignatura: Paradigmas de la programacion
 Rut: 19.684.813-4
+% VERSIÓN SWI-PROLOG: 8.4.2
 
 
 -------------------------Constructores-------------------
@@ -41,6 +42,7 @@ Meta secundaria: imagen
 
 
 /*Constructor pixbit-d
+Predicado pixbit-d
 Dom: X(int) X Y(int) X Bit([0|1]) X Depth(int)
 Rec: pixbit-d
 Ejemplo uso: 
@@ -50,6 +52,7 @@ pixbit-d(X, Y, Bit, Depth, [X,Y,Bit,Depth]).
 
 
 /*Constructor pixrgb-d
+Predicado pixrgb.d
 Dom: X(int) X Y(int) X R(C) X G(C) X B(C) X D
 Rec: pixrgb-d
 Ejemplo uso:
@@ -58,8 +61,28 @@ pixrgb-d(10, 10, 255, 255,255, 10, Lista), Devuelve Lista = [10, 10, 255, 255, 2
 pixrgb-d(X, Y, R, G, B, D, [X, Y, R, G, B, D]).
 
 /*Constructor pixhex-d
+Predicado pixhex-d
 Dom: Dominio: X(int) X Y(int) X hex(string) X D(int)
 Rec: pixhex-d
 Ejemplo uso: pixhex-d(10, 10, "#FF5733", 30, Lista), Devuelve lista = [10, 10, "#FF5733", 30].
 */
 pixhex-d(X, Y, Hex, D, [X, Y, Hex, D]).
+
+
+/*Constructor image
+Predicado image
+Dom: Width(int) X Height(int) X [pixbit-d | pixrgb-d | pixhex-d] Aridad 5
+Rec: image
+Ejemplo uso: 
+pixbit-d(10,20,1,25,L1),
+pixbit-d(5,10,0,20,L2),
+pixbit-d(0,50,1,4,L3),
+pixbit-d(10,10,0,22,L4),
+image(10,10,[L1,L2,L3,L4],LG)., Devuelve listaGeneral con todos los parametros dados
+*/
+
+image(Width, Height, ListPixel, [Width, Height, ListPixel]).
+
+
+
+
