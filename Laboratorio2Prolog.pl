b27488c9c8a4ac71e5d8f721f806b40d814293d4
Alex Mellado <alex.mellado.g@usach.cl>
2022-10-31 23:25:02 -0300

    Ejemplos de uso script principal

--- Laboratorio2Prolog.pl
+++ Laboratorio2Prolog.pl
@@ -60,6 +60,8 @@ Dom: X(int) X Y(int) X R(C) X G(C) X B(C) X D
 Rec: pixrgb-d
 Ejemplo uso:
 pixrgb-d(10, 10, 255, 255,255, 10, Lista), Devuelve Lista = [10, 10, 255, 255, 255, 10].
+pixrgb-d(5, 2, 55, 25,250, 4, Lista), Devuelve Lista = [5, 2, 55, 25, 250, 4].
+pixrgb-d(10, 10, 25, 25,25, 10, Lista), Devuelve Lista = [10, 10, 25, 25, 25, 10].
 */
 pixrgbD(X, Y, R, G, B, D, [X, Y, R, G, B, D]).
 
@@ -67,7 +69,10 @@ pixrgbD(X, Y, R, G, B, D, [X, Y, R, G, B, D]).
 Predicado pixhex-d
 Dom: Dominio: X(int) X Y(int) X hex(string) X D(int)
 Rec: pixhex-d
-Ejemplo uso: pixhex-d(10, 10, "#FF5733", 30, Lista), Devuelve lista = [10, 10, "#FF5733", 30].
+Ejemplo uso: 
+pixhex-d(10, 10, "#FF5733", 30, Lista), Devuelve lista = [10, 10, "#FF5733", 30].
+pixhex-d(1, 12, "#F08080", 35, Lista), Devuelve lista = [1, 12, "#F08080", 35].
+pixhex-d(2, 5, "#FA8072", 20, Lista), Devuelve lista = [2, 5, "#FA8072", 20].
 */
 pixhexD(X, Y, Hex, D, [X, Y, Hex, D]).
 
@@ -160,3 +165,21 @@ imagenIsAHexmap(Imagen):-
     image(_,_,Pixel,Imagen),
     isAPixhex(Pixel),
     imagenIsAHexmap(Pixel).
+
+
+
+/*Script de pruebas de los codigos hasta el momento empleados
+pixbitD(10, 0, 1, 1, Lista). Devuelve Lista = [10, 0, 1, 1].
+pixbitD(10, 5, 0, 5, Lista). Devuelve Lista = [10, 5, 0, 5].
+pixbitD(20, 20, 0, 10, Lista). Devuelve Lista = [20, 20, 0, 10].
+pixrgbD(10, 10, 255, 255,255, 10, Lista), Devuelve Lista = [10, 10, 255, 255, 255, 10].
+pixrgbD(5, 2, 55, 25,250, 4, Lista), Devuelve Lista = [5, 2, 55, 25, 250, 4].
+pixrgbD(10, 10, 25, 25,25, 10, Lista), Devuelve Lista = [10, 10, 25, 25, 25, 10].
+pixhexD(10, 10, "#FF5733", 30, Lista), Devuelve lista = [10, 10, "#FF5733", 30].
+pixhexD(1, 12, "#F08080", 35, Lista), Devuelve lista = [1, 12, "#F08080", 35].
+pixhexD(2, 5, "#FA8072", 20, Lista), Devuelve lista = [2, 5, "#FA8072", 20].
+pixbitD(10,20,1,25,L1),pixbitD(5,10,0,20,L2),pixbitD(0,50,1,4,L3),pixbitD(10,10,0,22,L4), image(10,10,[L1,L2,L3,L4],I). Devuelve I con los parametros dados
+pixbitD(15,22,0,22,L1),pixbitD(1,1,0,30,L2),pixbitD(5,50,0,10,L3),pixbitD(10,10,1,50,L4), image(10,2,[L1,L2,L3,L4],I). Devuelve I con los parametros dados
+pixhexD(15,22,"#F08080",22,L1),pixhexD(1,1,"#FA8072",30,L2),pixhexD(5,50,"#FF5733",10,L3),pixhexD(10,10,"#FF5733",50,L4), image(10,2,[L1,L2,L3,L4],I). Devuelve I con los parametros dados
+(pixbitD( 0, 0, 1, 10, PA), pixbitD( 0, 1, 0, 20, PB), pixbitD( 1, 0, 0, 30, PC), pixbitD( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), 
+imagenABitmap(I)). Retorna falso, hay que revisar la condición para verificar.
