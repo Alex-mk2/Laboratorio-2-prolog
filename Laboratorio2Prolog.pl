51fa951b71c03daa51db9eac1a2766fec92b81e7
Alex Mellado <alex.mellado.g@usach.cl>
2022-11-03 18:31:37 -0300

    Avance predicado compress y correcion de los otros predicados realizados previamente

--- Laboratorio2Prolog.pl
+++ Laboratorio2Prolog.pl
@@ -89,9 +89,9 @@ Predicado pixhex-d
 Dom: Dominio: X(int) X Y(int) X hex(string) X D(int)
 Rec: pixhex-d
 Ejemplo uso: 
-pixhex-d(10, 10, "#FF5733", 30, Lista), Devuelve lista = [10, 10, "#FF5733", 30].
-pixhex-d(1, 12, "#F08080", 35, Lista), Devuelve lista = [1, 12, "#F08080", 35].
-pixhex-d(2, 5, "#FA8072", 20, Lista), Devuelve lista = [2, 5, "#FA8072", 20].
+pixhexD(10, 10, "#FF5733", 30, Lista), Devuelve lista = [10, 10, "#FF5733", 30].
+pixhexD(1, 12, "#F08080", 35, Lista), Devuelve lista = [1, 12, "#F08080", 35].
+pixhexD(2, 5, "#FA8072", 20, Lista), Devuelve lista = [2, 5, "#FA8072", 20].
 */
 pixhexD(X, Y, Hex, D, [X, Y, Hex, D]).
 
@@ -119,18 +119,18 @@ imagen(Alto, Ancho, ListaPixeles, [Alto, Ancho, ListaPixeles]).
 
 
 /*Selectores  TDA Image*/
-obtenerAlto([Width|_], Width).
-obtenerAncho([_|[Height|_]], Height).
+obtenerAlto([Alto|_], Alto).
+obtenerAncho([_|[Ancho|_]], Ancho).
 obtenerListaPixelImagen([_|[_|[ListaPixeles|_]]], ListaPixeles).
 obtenerPixeles([_|[_|[_|[Pixeles|_]]]], Pixeles).
 
-/*Predicado para verificar la pertenencia en un pixbit-d
+/*Predicado para verificar la pertenencia en un pixbitD
 Dom: pixrgb-d 
 Meta: Saber si es un bitmap
 */
-isABitmap(Pixbit).
-isABitmap([Pixbitd|Cola]):-
-    getBit(Pixbit, Bit),
+isABitmap([]).
+isABitmap(Pixbit):-
+    obtenerBit(Pixbit, Bit),
     (Bit==0 ; Bit==1).
 
 /*Predicado complemento de isABitmap
@@ -138,7 +138,9 @@ Dom: lista
 Meta: Verificar en la lista el bitmap*/
 
 verificarBitmap([]).
-verificarBitmap([X|Y]):- isABitmap(X),verificarBitmap(Y).
+verificarBitmap([Cabeza|Resto]):- 
+    isABitmap(Cabeza),
+    verificarBitmap(Resto).
 
 
 /*Predicado para verificar si existe bitmap en una imagen
@@ -151,35 +153,57 @@ pixbitD( 1, 0, 0, 30, PC),
 pixbitD( 1, 1, 1, 4, PD), 
 image( 2, 2, [PA, PB, PC, PD], I), 
 imagenABitmap(I)).
-Retorna Falso, hay que revisar la condicion
+
+(((pixhexD( 0, 0, "#FF5733", 10, PA), 
+         pixhexD( 0, 1, "#FF5733", 20, PB), 
+         pixhexD( 1, 0, 1, 30, PC), 
+         pixhexD( 1, 1, 1, 4, PD), 
+         imagen(2,2,[PA, PB, PC, PD],I), 
+imagenABitmap(I)))).
+Retorna Falso.
 */
 
-imagenABitmap([]).
-imagenABitmap(Imagen):- getListPixelImage(Imagen, Pixeles), verificarBitmap(Pixeles).
+imagenIsABitmap([]).
+imagenIsBitmap(Imagen):- 
+    obtenerListaPixelImagen(Imagen, ListaPixeles), 
+    verificarBitmap(ListaPixeles).
 
 
-/*Predicado para verificar si existe pixrgb en una imagen
+
+/*Predicado para verificar si existe pixrgb
  Dom: pixrgb
- Rec: Booleano
+ Meta: Verificar si es un pixrgb con las condiciones dadas
 */
 isAPixrgbD([]).
-isAPixrgbD([Pixrgbd|Cola]):-
-    pixrgbD(_,_,R,G,B,_, Pixrgbd),
-    (R =< 0, R =< 255),
-    (G =< 0, G =< 255),
-    (B =< 0, B =< 255),
-    isAPixrgbD(Cola).
+isAPixrgbD(Pixrgbd):-
+           obtenerR(Pixrgbd, R),
+           obtenerG(Pixrgbd, G),
+           obtenerB(Pixrgbd, B),
+           (R >= 0, R =< 255),
+           (G >= 0, G =< 255),
+           (B >= 0, B =< 255).
+
+/*Predicado para verificar Pixmap
+ * Dom: lista
+ * Meta: Como el predicador para verificar bitmap, este seria para pixmap
+ * y la meta sería la misma, verificar si es un pixmap
+*/
+verificarPixmap([]).
+verificarPixmap([Cabeza|Resto]):- 
+    isAPixrgbD(Cabeza),
+    verificarPixmap(Resto).
 
 /*Predicado para verificar si existe un pixmap
  * Dom: pixmap
- * Meta: Verificar si existe el pixmap (verdadero o falso).
+ * Meta: Verificar si existe el pixmap (verdadero o falso) a traves de los predicados
+ * complementos.
 */
 
 imagenIsAPixmap([]).
 imagenIsAPixmap(Imagen):-
-    image(_,_,Pixel,Imagen),
-    isAPixrgbD(Pixel),
-    imagenIsAPixmap(Pixel).
+           obtenerListaPixelImagen(Imagen, ListaPixeles),
+           verificarPixmap(ListaPixeles).
+           
 
 /*Predicado para verificar si existe pixhex en una imagen
  * Dom: pixhexD
@@ -187,10 +211,21 @@ imagenIsAPixmap(Imagen):-
 */
 
 isAPixhex([]).
-isAPixhex([Pixhex|Cola]):-
-    pixhexD(_,_,Pixhex,_,Hex),
-    Hex = Pixhex,
-    isAPixhex(Cola).
+isAPixhex(Pixhex):-
+    obtenerHex(Pixhex, Hex),
+    string(Hex).
+    
+/*Predicado para verificar 
+ * Dom: lista
+ * Meta: De igual manera que los predicados verificarPixmap o verificarBitmap
+ * Opera de la misma manera.
+*/
+
+verificarHexmap([]).
+verificarHexmap([Cabeza|Resto]):-
+    isAPixhex(Cabeza),
+    verificarHexmap(Resto).
+   
 
 /*Predicado para verificar un hexmap
  * Dom: PixhexD
@@ -199,9 +234,54 @@ isAPixhex([Pixhex|Cola]):-
 
 imagenIsAHexmap([]).
 imagenIsAHexmap(Imagen):-
-    image(_,_,Pixel,Imagen),
-    isAPixhex(Pixel),
-    imagenIsAHexmap(Pixel).
+    obtenerListaPixelImagen(Imagen, ListaPixeles),
+    verificarHexmap(ListaPixeles).
+
+
+/*Predicado complemento compress? verificar si se puede comprimir una imagen
+ * Dom: Lista
+ * Meta: complementar el predicado compress?
+*/
+isACompress([]).
+isACompress(Compress):-
+    obtenerAlto(Compress, Alto),
+    obtenerAncho(Compress,Ancho),
+    obtenerListaPixelImagen(Compress, ListaPixeles),
+    (ListaPixeles < Alto * Ancho).
+    
+    
+/*Predicado complemento para verificar isAcompress
+ * Dom: Lista
+ * Meta: Complemento al predicado IsAcompress
+*/
+
+verificarCompress([]).
+verificarCompress([Cabeza|Resto]):-
+    isACompress(Cabeza),
+    verificarCompress(Resto).
+
+/*Predicado para saber si una imagen se puede comprimir o no
+ * Dom: Compress y complementos
+ * Meta: Saber si es posible comprimir una imagen
+*/
+
+compress([]).
+compress(Imagen):-
+    obtenerListaPixelImagen(Imagen, ListaPixeles),
+    verificarCompress(ListaPixeles).
+
+    
+
+/*Predicado pertenencia
+ * De aqui surgio la idea del predicado verificar
+ * Dom: elemento X elemento
+ * Meta: Verificar si ese elemento existe
+*/
+
+pertenece(Elemento, [Elemento|_] ).
+pertenece(Elemento, [_|Resto]):-
+	pertenece(Elemento, Resto).
+
 
 
 /*Script de pruebas de los codigos hasta el momento empleados
@@ -217,7 +297,80 @@ pixhexD(2, 5, "#FA8072", 20, Lista), Devuelve lista = [2, 5, "#FA8072", 20].
 pixbitD(10,20,1,25,L1),pixbitD(5,10,0,20,L2),pixbitD(0,50,1,4,L3),pixbitD(10,10,0,22,L4), imagen(10,10,[L1,L2,L3,L4],I). Devuelve I con los parametros dados
 pixbitD(15,22,0,22,L1),pixbitD(1,1,0,30,L2),pixbitD(5,50,0,10,L3),pixbitD(10,10,1,50,L4), imagen(10,2,[L1,L2,L3,L4],I). Devuelve I con los parametros dados
 pixhexD(15,22,"#F08080",22,L1),pixhexD(1,1,"#FA8072",30,L2),pixhexD(5,50,"#FF5733",10,L3),pixhexD(10,10,"#FF5733",50,L4), imagen(10,2,[L1,L2,L3,L4],I). Devuelve I con los parametros dados
-(pixbitD( 0, 0, 1, 10, PA), pixbitD( 0, 1, 0, 20, PB), pixbitD( 1, 0, 0, 30, PC), pixbitD( 1, 1, 1, 4, PD), imagen( 2, 2, [PA, PB, PC, PD], I), 
-imagenABitmap(I)). Retorna falso, hay que revisar la condición para verificar el caso pixbitD.
-(((pixhexD( 0, 0, "#FF5733", 10, PA), pixhexD( 0, 1, "#FF5733", 20, PB), pixhexD( 1, 0, 1, 30, PC), pixhexD( 1, 1, 1, 4, PD), imagen(2,2,[PA, PB, PC, PD],I), imagenABitmap(I)))).
-Retorna Falso.
+
+
+----------------------------------------------Pertenencias Bitmap----------------------------------------------------------------------------------------------------------
+
+(pixbitD( 0, 0, 1, 10, PA), 
+pixbitD( 0, 1, 0, 20, PB), 
+pixbitD( 1, 0, 0, 30, PC), 
+pixbitD( 1, 1, 1, 4, PD), 
+imagen( 2, 2, [PA, PB, PC, PD], I), 
+imagenABitmap(I)). 
+Retorna verdadero, cumple con la condición de bit
+
+(((pixhexD( 0, 0, "#FF5733", 10, PA), 
+pixhexD( 0, 1, "#FF5733", 20, PB), pixhexD( 1, 0, 1, 30, PC), 
+pixhexD( 1, 1, 1, 4, PD), 
+imagen(2,2,[PA, PB, PC, PD],I), 
+imagenABitmap(I)))).
+Retorna Falso, cumple con la condición de bit.
+
+(pixrgbD( 0, 0, 4, 10,10, 10, PA), pixrgbD( 0, 1, 4, 20,20, 20,PB), 
+pixrgbD( 1, 0, 4, 30,30,30, PC), 
+pixrgbD( 1, 1, 4, 4,4,4, PD), 
+imagen( 2, 2, [PA, PB, PC, PD], I), 
+imagenABitmap(I)). Retorna falso, cumple con la condicion de bit
+
+--------------------------------------------------------------------Pertenencias Pixmap-------------------------------------------------------------------------------------
+
+(pixbitD( 0, 0, 1, 10, PA), 
+pixbitD( 0, 1, 0, 20, PB), 
+pixbitD( 1, 0, 0, 30, PC), 
+pixbitD( 1, 1, 1, 4, PD), 
+imagen( 2, 2, [PA, PB, PC, PD], I), 
+imagenIsAPixmap(I)). Retorna falso, cumple con la condición de pixmap
+
+(pixhexD( 0, 0, "#FA8072", 10, PA), pixhexD( 0, 1, 0, 20, PB), 
+pixhexD( 1, 0, 0, 30, PC), 
+pixhexD( 1, 1, "#FA8072", 4, PD), 
+imagen( 2, 2, [PA, PB, PC, PD], I), 
+imagenIsAPixmap(I)). Retorna falso, cumple con la condicion de pixmap
+
+(pixrgbD( 0, 0, 4, 10,10, 10, PA), pixrgbD( 0, 1, 4, 20,20, 20,PB), 
+pixrgbD( 1, 0, 4, 30,30,30, PC), 
+pixrgbD( 1, 1, 4, 4,4,4, PD), 
+imagen( 2, 2, [PA, PB, PC, PD], I), 
+imagenIsAPixmap(I)). Retorna verdadero, cumple con la condición de pixmap
+
+
+
+--------------------------------------------------------------Pertenencia Hexmap--------------------------------------------------------------
+
+(pixrgbD( 0, 0, 4, 10,10, 10, PA), 
+pixrgbD( 0, 1, 4, 20,20, 20,PB), 
+pixrgbD( 1, 0, 4, 30,30,30, PC), 
+pixrgbD( 1, 1, 4, 4,4,4, PD), 
+imagen( 2, 2, [PA, PB, PC, PD], I), 
+imagenIsAHexmap(I)). Retorna Falso, no es un hexmap
+
+(pixbitD( 0, 0, 4, 10, PA), pixbitD( 0, 1, 4, 10,PB), 
+pixbitD( 1, 0, 4, 30,PC), 
+pixbitD( 1, 1, 4, 4, PD), 
+imagen( 2, 2, [PA, PB, PC, PD], I), 
+imagenIsAHexmap(I)). Retorna falso, no es un hexmap
+
+(pixhexD( 0, 0, "#FF5733", 10, PA), pixhexD( 0, 1, "#FF5733", 10,PB), 
+pixhexD( 1, 0, "#FF5733", 30,PC), 
+pixhexD( 1, 1, "#FF5733", 4, PD), 
+imagen( 2, 2, [PA, PB, PC, PD], I), 
+imagenIsAHexmap(I)). Cumple con la condición
+
+
+
+------------------------------------------------------------Compress---------------------------------------------------------------------------
+(pixbitD( 0, 0, 20, 10, PA), pixbitD( 0, 1, 20, 10,PB), 
+pixbitD( 1, 2, 20, 30,PC), 
+pixbitD( 1, 1, 20, 4, PD), 
+imagen( 2, 2, [PA, PB, PC, PD], I), 
+compress(I)). Tira falso
